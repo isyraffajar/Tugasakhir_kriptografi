59b13acf94b1f50e0b0cf903b9cfae6dbd47582e
isyraffajar <itopfajar@gmail.com>
2025-11-07 16:07:04 +0700

    Tambah session sama logout

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from backend.auth import register_user,login_user
 
 app = Flask(__name__, template_folder='frontend')
+app.secret_key = 'kunci_session_bebas'
 
 @app.route("/")
 def home():
@@ -9,6 +10,9 @@ def home():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    # Jika user sudah login, arahkan ke index
+    if 'user_email' in session:
+        return redirect(url_for('index'))
     error = None
     email = ""
     if request.method == 'POST':
@@ -17,6 +21,8 @@ def login():
 
         success, message = login_user(email, password)
         if success:
+            # SET SESSION
+            session['user_email'] = email
             return redirect(url_for('index'))
         else:
             error = message
@@ -25,6 +31,9 @@ def login():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
+    # Jika user sudah login, arahkan ke index
+    if 'user_email' in session:
+        return redirect(url_for('index'))
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
@@ -50,7 +59,16 @@ def register():
 
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    # Cek apakah user sudah login
+    if 'user_email' not in session:
+        return redirect(url_for('login'))  # jika belum login, arahkan ke login
+    return render_template("index.html", user_email=session['user_email'])
+
+@app.route("/logout")
+def logout():
+    # Hapus session user
+    session.pop('user_email', None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
